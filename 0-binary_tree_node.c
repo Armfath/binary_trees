5006a6aea5947562aaabe6ef1c33370e4c7a1e36
Armfath <info.armanagonou@gmail.com>
2023-02-28 14:52:21 +0100

    wrap else

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,13 +4,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = calloc(1, sizeof(binary_tree_t));
 	if (new_node != NULL)
 	{
 		new_node->parent = parent;
 		new_node->n = value;
-		new_node->left = NULL;
-		new_node->right = NULL;
 		if (parent != NULL)
 		{
 			if (parent->left == NULL)
@@ -22,8 +20,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 				parent->right = new_node;
 			}
 			else
+			{
 				free(new_node);
 				return (NULL);
+			}
 		}
 
 		return (new_node);
