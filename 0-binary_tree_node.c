680340bc9a1538b56ad88e7d20d3ff3010e06c8d
Armfath <info.armanagonou@gmail.com>
2023-02-28 14:46:42 +0100

    fix 00

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,17 +4,27 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = calloc(1, sizeof(binary_tree_t));
 	if (new_node != NULL)
 	{
 		new_node->parent = parent;
 		new_node->n = value;
-		new_node->left = NULL;
-		new_node->left = NULL;
+		if (parent != NULL) {
+			if (parent->left == NULL)
+			{
+				parent->left = new_node;
+			}
+			else if (parent->right == NULL)
+			{
+				parent->right = new_node;
+        	}
+			else
+				free(new_node);
+				return (NULL);
+    	}
 
 		return (new_node);
 	}
 	free(new_node);
-
 	return (NULL);
 }
